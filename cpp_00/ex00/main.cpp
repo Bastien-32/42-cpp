7698f709c8ef28d3a402051b8939c841a6e4cb21
Bastien-32 <bastiendallago@hotmail.fr>
2025-06-26 12:01:21 +0200

    ex00 ok

---
+++ cpp_00/ex00/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: badal-la <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/26 10:47:31 by badal-la          #+#    #+#             */
+/*   Updated: 2025/06/26 11:03:40 by badal-la         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			std::string str = argv[i];
+			for(int j = 0; j < str.length(); j++)
+				std::cout << (char)std::toupper(str[j]);
+		}
+		std::cout << std::endl;
+	}
+	return (0);
+}
---
+++ cpp_00/ex01/PhoneBook.class.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.class.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: badal-la <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/26 11:34:25 by badal-la          #+#    #+#             */
+/*   Updated: 2025/06/26 11:48:03 by badal-la         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_CLASS_H
+# define PHONEBOOK_CLASS_H
+
+class PhoneBook{
+
+public:
+
+	PhoneBook( void );
+	~PhoneBook( void );
+
+};
+
+#endif
