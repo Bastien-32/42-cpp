db993704dd04cbca0f53062cbc25528915f8cd8f
Bastien <bastiendallago@hotmail.fr>
2025-06-28 17:06:59 +0200

    ex02 in progress

---
+++ cpp_00/ex02/src/Account.cpp
@@ -0,0 +1,108 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: badal-la <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/28 11:32:34 by badal-la          #+#    #+#             */
+/*   Updated: 2025/06/28 17:06:21 by badal-la         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/Account.hpp"
+#include "../include/colors.hpp"
+#include <iostream>
+#include <iomanip>
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+Account::Account(int initial_deposit) :
+	_accountIndex(_nbAccounts++),
+	_amount(initial_deposit),
+	_nbDeposits(0),
+	_nbWithdrawals(0)
+{
+	_totalAmount += initial_deposit;
+	_displayTimestamp();
+	std::cout << "index:"
+				<< BLUE << _accountIndex << RESET
+				<< ";amount:"
+				<< BLUE << _amount << RESET
+				<< ";created" << std::endl;
+}
+
+Account::~Account()
+{
+	_displayTimestamp();
+	std::cout << "index:"
+				<< BLUE << _accountIndex << RESET
+				<< ";amount:"
+				<< BLUE << _amount << RESET
+				<< ";closed" << std::endl;
+}
+
+void Account::_displayTimestamp() {
+	std::time_t now = std::time(nullptr);
+	std::tm *ltm = std::localtime(&now);
+
+	std::cout << "[" << 1900 + ltm->tm_year
+				<< std::setw(2) << std::setfill('0') << ltm->tm_mon + 1
+				<< std::setw(2) << std::setfill('0') << ltm->tm_mday
+				<< "_"
+				<< std::setw(2) << std::setfill('0') << ltm->tm_hour
+				<< std::setw(2) << std::setfill('0') << ltm->tm_min
+				<< std::setw(2) << std::setfill('0') << ltm->tm_sec
+				<< "] ";
+}
+
+void Account::displayAccountsInfos() {
+	_displayTimestamp();
+	std::cout << "accounts:"
+				<< BLUE << _nbAccounts << RESET
+				<< ";total:"
+				<< BLUE << _totalAmount << RESET
+				<< ";deposits:"
+				<< BLUE << _totalNbDeposits << RESET
+				<< ";withdrawals:"
+				<< BLUE << _totalNbWithdrawals << RESET
+				<< std::endl;
+}
+
+void Account::displayStatus() const {
+	for(int i = 0; i < _nbAccounts; ++i) {
+		_displayTimestamp();
+		std::cout << "index:"
+					<< BLUE << _accountIndex << RESET
+					<< ";amount:"
+					<< BLUE << _amount << RESET
+					<< ";deposits:"
+					<< BLUE << _nbDeposits << RESET
+					<< ";withdrawals:"
+					<< BLUE << _nbWithdrawals << RESET
+					<< std::endl;
+		break;
+	}
+}
+
+void Account::makeDeposit(int deposit) {
+	_displayTimestamp();
+	int	p_amount(_amount);
+	_amount += deposit;
+	std::cout << "index:"
+				<< BLUE << _accountIndex << RESET
+				<< ";p_amount:"
+				<< BLUE << p_amount << RESET
+				<< ";deposit:"
+				<< BLUE << deposit << RESET
+				<< ";amount:"
+				<< BLUE << _amount << RESET
+				<< ";nb_deposits:"
+				<< BLUE << ++_nbDeposits << RESET
+				<< std::endl;
+	_totalAmount += deposit;
+	_totalNbDeposits++;
+}
