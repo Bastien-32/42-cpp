d3e697b3ba386588a2b2e0f7f9f38bdec81e4923
Bastien-32 <bastiendallago@hotmail.fr>
2025-08-20 17:10:44 +0200

    cpp_08 ex00

--- cpp_08/ex00/include/easyfind.hpp
+++ cpp_08/ex00/include/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: badal-la <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/07 16:12:03 by badal-la          #+#    #+#             */
-/*   Updated: 2025/08/20 15:08:34 by badal-la         ###   ########.fr       */
+/*   Updated: 2025/08/20 17:09:57 by badal-la         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ typename T::iterator	easyfind(T& container, int value)
 
 	it = std::find(container.begin(), container.end(), value);
 	if (it == container.end())
-		throw std::runtime_error("value not found");
+		throw std::runtime_error("Value not found!");
 	return (it);
 }
 
